afbdef40d4e3906143c39eda1fbaddb9a9dae917
Tatiana <tany663@gmail.com>
2026-02-19 20:25:32 +0200

    add metadata url

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
@@ -7,25 +7,25 @@ import { Roboto } from 'next/font/google';
 
 
 
-// export const metadata: Metadata = {
-//   title: 'Note Hub',
-//   description: 'My note hub',
-//    openGraph: {
-//       title: 'Note Hub',
-//       description: 'My note hub',
-//       url: '',
-//       siteName: 'NoteHub',
-//       images: [
-//         {
-//           url:  'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
-//           width: 1200,
-//           height: 630,
-//           alt: 'Note Hub Open Graph Image',
-//         },
-//       ],
-//       type: 'article',
-//     }
-// };
+export const metadata: Metadata = {
+  title: 'Note Hub',
+  description: 'My note hub',
+   openGraph: {
+      title: 'Note Hub',
+      description: 'My note hub',
+      url: 'https://08-zustand-gray-zeta.vercel.app',
+      siteName: 'NoteHub',
+      images: [
+        {
+          url:  'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'Note Hub Open Graph Image',
+        },
+      ],
+      type: 'article',
+    }
+};
 
 const roboto = Roboto({
   subsets: ['latin'], 
